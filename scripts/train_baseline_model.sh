76f85215460760fa48d254530e5e1ebc1429fa5b
xjanik20 <luo96@seznam.cz>
2022-11-15 01:59:25 +0100

    Increase CUDA cap to get faster GPUs

--- scripts/train_baseline_model.sh
+++ scripts/train_baseline_model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -q gpu
-#PBS -l select=1:ncpus=12:ngpus=2:gpu_cap=cuda75:gpu_mem=10gb:mem=20gb:scratch_ssd=10gb
+#PBS -l select=1:ncpus=12:ngpus=2:gpu_cap=cuda80:gpu_mem=10gb:mem=20gb:scratch_ssd=10gb
 #PBS -j oe
 
 # Author: Roman Janík
