72b3541519bd9f2d33ba6bdbd1a35fd942949272
Roman Janík <luo96@seznam.cz>
2023-03-10 16:38:26 +0100

    Update prepare_cnec_chnec_datasets.py

--- scripts/prepare_cnec_chnec_datasets.py
+++ scripts/prepare_cnec_chnec_datasets.py
@@ -25,45 +25,47 @@ cnec_dir = "cnec2.0_extended"
 chnec_dir = "chnec1.0"
 sumeczech_dir = "sumeczech-1.0-ner"
 
-# # load CNEC 2.0 CoNLL dataset with loading script
-# cnec_dataset = datasets.load_dataset("cnec2_0_conll.py")
-#
-# # transform CNEC 2.0 CoNLL dataset into CHNEC 1.0 format -> change in NER tags
-# cnec_dataset = cnec_dataset.cast_column("ner_tags", datasets.Sequence(
-#     datasets.ClassLabel(
-#         names=[
-#             "O",
-#             "B-p",
-#             "I-p",
-#             "B-i",
-#             "I-i",
-#             "B-g",
-#             "I-g",
-#             "B-t",
-#             "I-t",
-#             "B-o",
-#             "I-o"
-#         ]
-#     )
-# )
-#                                         )
-# cnec_dataset = cnec_dataset.map(lower_ner_tag_types, batched=True)
-#
-# # remove columns irrelevant to NER task
-# cnec_dataset = cnec_dataset.remove_columns(["lemmas", "morph_tags"])
-#
-# # save CNEC 2.0 CoNLL dataset in Hugging Face Datasets format (not tokenized)
-# cnec_dataset.save_to_disk(os.path.join(datasets_path, cnec_dir))
-#
-#
-# # load CHNEC 1.0 dataset with loading script
-# chnec_dataset = datasets.load_dataset("chnec1_0.py")
-#
-# # remove columns irrelevant to NER task
-# chnec_dataset = chnec_dataset.remove_columns(["lemmas", "language"])
-#
-# # save CHNEC 1.0 dataset in Hugging Face Datasets format (not tokenized)
-# chnec_dataset.save_to_disk(os.path.join(datasets_path, chnec_dir))
+# load CNEC 2.0 CoNLL dataset with loading script
+cnec_dataset = datasets.load_dataset("cnec2_0_conll.py")
+print(cnec_dataset["train"][71]["tokens"])
+print(cnec_dataset["train"][71]["ner_tags"])
+
+# transform CNEC 2.0 CoNLL dataset into CHNEC 1.0 format -> change in NER tags
+cnec_dataset = cnec_dataset.cast_column("ner_tags", datasets.Sequence(
+    datasets.ClassLabel(
+        names=[
+            "O",
+            "B-p",
+            "I-p",
+            "B-i",
+            "I-i",
+            "B-g",
+            "I-g",
+            "B-t",
+            "I-t",
+            "B-o",
+            "I-o"
+        ]
+    )
+)
+                                        )
+cnec_dataset = cnec_dataset.map(lower_ner_tag_types, batched=True)
+
+# remove columns irrelevant to NER task
+cnec_dataset = cnec_dataset.remove_columns(["lemmas", "morph_tags"])
+
+# save CNEC 2.0 CoNLL dataset in Hugging Face Datasets format (not tokenized)
+cnec_dataset.save_to_disk(os.path.join(datasets_path, cnec_dir))
+
+
+# load CHNEC 1.0 dataset with loading script
+chnec_dataset = datasets.load_dataset("chnec1_0.py")
+
+# remove columns irrelevant to NER task
+chnec_dataset = chnec_dataset.remove_columns(["lemmas", "language"])
+
+# save CHNEC 1.0 dataset in Hugging Face Datasets format (not tokenized)
+chnec_dataset.save_to_disk(os.path.join(datasets_path, chnec_dir))
 
 
 # load SumeCzech-NER 1.0 dataset with loading script
