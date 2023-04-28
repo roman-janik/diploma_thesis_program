8fdf9b819fa47f2fef6b0b16414c9295946f60bb
Roman Janík <luo96@seznam.cz>
2023-04-28 13:37:56 +0200

    Change train baseline num cpus and gpus

--- scripts/train_baseline_model.sh
+++ scripts/train_baseline_model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -q gpu
-#PBS -l select=1:ncpus=8:ngpus=2:gpu_cap=cuda80:gpu_mem=20gb:mem=20gb:scratch_ssd=20gb:cluster=galdor
+#PBS -l select=1:ncpus=2:ngpus=1:gpu_cap=cuda80:gpu_mem=20gb:mem=20gb:scratch_ssd=20gb:cluster=galdor
 #PBS -j oe
 
 # Author: Roman Janík
